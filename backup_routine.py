4f438cccffd6cb313e8d2a818937bdacfcb83026
Yuan-Yi Chang <changyy.csie@gmail.com>
2013-06-16 11:57:11 +0800

    init

---
+++ backup_routine.py
@@ -0,0 +1,13 @@
+import json
+import urllib2
+import os
+
+backup_path = "~/Dropbox/git-rep/github"
+username = "changyy"
+github_api = "https://api.github.com/users/"+username+"/repos"
+
+raw = urllib2.urlopen(github_api).read()
+parser = json.loads(raw)
+for repo in parser:
+	print "Target:" + repo["clone_url"]
+	os.system( "cd " + backup_path + " && (( test -e " + repo["name"] + " && cd " + repo["name"] + " && git pull ) ||  git clone " + repo["clone_url"] + " ) " )
